82594782b1b178ac724dcdabbe289e1883784ec7
Anh Nguyen <avtnguyen1@gmail.com>
2024-08-31 10:55:51 -0400

    add file add vegan.py

--- VeganProj/addVeganCal.py
+++ VeganProj/addVeganCal.py
@@ -7,15 +7,18 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 
-def main():
-  """Shows basic usage of the Google Calendar API.
-  Prints the start and name of the next 10 events on the user's calendar.
+def addToGoogleCalendar(calendar_name, event_description, date_start, date_end, 
+                        first_reminder_type, first_reminder_time, 
+                        second_reminder_type, second_reminder_time):
   """
+  Add an all day event in google calendar using google calendar api
+  """
+  # If modifying these scopes, delete the file token.json.
+  SCOPES = ["https://www.googleapis.com/auth/calendar"]
   creds = None
+
   # The file token.json stores the user's access and refresh tokens, and is
   # created automatically when the authorization flow completes for the first
   # time.
@@ -37,34 +40,70 @@ def main():
   try:
     service = build("calendar", "v3", credentials=creds)
 
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    print("Getting the upcoming 10 events")
-    events_result = (
-        service.events()
-        .list(
-            calendarId="primary",
-            timeMin=now,
-            maxResults=10,
-            singleEvents=True,
-            orderBy="startTime",
-        )
-        .execute()
-    )
-    events = events_result.get("items", [])
+    
+    # Feature 1: Check if vegan calendar is in the list, if not add the new calendar
+    #vegan_calendar = 'Vegan Calendar'
+    print("Fetching all calendars:")
+    calendar_list = service.calendarList().list().execute().get('items', [])
+    flag = False
+    for calendar in calendar_list:
+        if calendar['summary'] == calendar_name:
+            flag = True
+            calendar_id = calendar['id']
+        print(calendar['summary'])
 
-    if not events:
-      print("No upcoming events found.")
-      return
+    # Feature 2: Create a new calendar
+    if flag == False:
+        new_calendar = {
+            'summary': calendar_name,
+            'timeZone': 'America/Toronto'
+        }
+        created_calendar = service.calendars().insert(body=new_calendar).execute()
+        calendar_id = created_calendar['id']
+        print(f"Created calendar: {calendar_id}")
+    else:
+       print(f'Calendar {calendar_name} is alread exist!')
 
-    # Prints the start and name of the next 10 events
-    for event in events:
-      start = event["start"].get("dateTime", event["start"].get("date"))
-      print(start, event["summary"])
-
+    # Feature 3: add an event in the vegan calendar
+    # Feature 3: Insert an event
+    event = {
+        'summary': calendar_name,
+        
+        'description': event_description,
+        'start': {
+            'date': date_start,
+            'timeZone': 'America/Toronto',
+        },
+        'end': {
+            'date': date_end,
+            'timeZone': 'America/Toronto',
+        },
+        'reminders':{
+            "useDefault": False,
+            "overrides": [
+            {
+                "method": first_reminder_type,
+                "minutes": first_reminder_time #3180
+            },
+            {
+               "method": second_reminder_type,
+                "minutes": second_reminder_time #1740
+            }]
+        }
+    }
+    created_event = service.events().insert(calendarId=calendar_id, body=event).execute()
+    
+    print(f"Created event: {created_event['id']}")
+    
+    
   except HttpError as error:
     print(f"An error occurred: {error}")
 
+addToGoogleCalendar(calendar_name = 'Vegan Calendar', event_description = 'Test 4', 
+                    date_start = '2024-08-31' , date_end = '2024-08-31', 
+                        first_reminder_type = 'email', first_reminder_time = 3180, 
+                        second_reminder_type = 'popup', second_reminder_time = 1740)
 
-if __name__ == "__main__":
-  main()
+
+#if __name__ == "__main__":
+#  main()
